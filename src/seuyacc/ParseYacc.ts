4a70a8d17f4dda4d617a57c3c00546708143215a
潘昱辰 <2718911600@qq.com>
2022-09-21 22:43:12 +0800

    添加LR1项集生成部分

--- src/seuyacc/ParseYacc.ts
+++ src/seuyacc/ParseYacc.ts
@@ -1,4 +1,4 @@
-import { Production } from "./YaccTypes"
+import { LR1Collection, LR1Item, Production } from "./YaccTypes"
 
 let FIRST = (
   productionList: Production[],
@@ -11,20 +11,20 @@ let FIRST = (
   while (true) {
     let flag = true
     productionList.forEach(production => {
-      if (production.right.length === 1 && production.right[0] === '') {
-        (ans.get(production.left) as Set<string>).add('')
+      if (production.getRight().length === 1 && production.getRight()[0] === '') {
+        (ans.get(production.getLeft()) as Set<string>).add('')
         return
       }
       let i = 0
       do {
-        (ans.get(production.right[i]) as Set<string>)
+        (ans.get(production.getRight()[i]) as Set<string>)
           .forEach(value => {
-            if (!(ans.get(production.left) as Set<string>).has(value)) {
+            if (!(ans.get(production.getLeft()) as Set<string>).has(value)) {
               flag = false
             }
-            (ans.get(production.left) as Set<string>).add(value)
+            (ans.get(production.getLeft()) as Set<string>).add(value)
           })
-      } while ((ans.get(production.right[i++]) as Set<string>).has('') && i < production.right.length)
+      } while ((ans.get(production.getRight()[i++]) as Set<string>).has('') && i < production.getRight().length)
     })
     if (flag)
       break
@@ -46,7 +46,7 @@ let FOLLOW = (
   while (true) {
     let flag = true
     for (let i = 0; i < productionList.length; i++) {
-      let { left, right } = productionList[i]
+      let [left, right] = [productionList[i].getLeft(), productionList[i].getRight()]
       for (let j = 0; j < right.length; j++) {
         if (j === right.length - 1 ||
           j === right.length - 2 &&
@@ -70,6 +70,77 @@ let FOLLOW = (
     }
     if (flag)
       break
+  }
+  return ans
+}
+
+let CLOSURE = (collection: LR1Collection, productionList: Production[], firstMap: Map<string, Set<string>>): LR1Collection => {
+  let ans = collection.deepCopy()
+  let idx = 0
+  let visitedNonTerminator: Set<string> = new Set()
+  while (idx < ans.getItem().length) {
+    let header = ans.getItem()[idx].getHeader()
+    let next = ans.getItem()[idx].getNext()?.getHeader()
+    let firstBetaAlpha: Set<string> = new Set()
+    if (!next) {
+      firstBetaAlpha.add(ans.getItem()[idx].getExpect())
+    }
+    else {
+      firstMap.get(next)?.forEach(value => firstBetaAlpha.add(value))
+      firstBetaAlpha.delete('')
+    }
+    if (header === null) {
+      idx++
+      continue
+    }
+    productionList
+      .filter(value => value.getLeft() === header)
+      .forEach(value => {
+        for (let [v, _v] of firstBetaAlpha.entries()) {
+          let temp = new LR1Item(value, 0, v)
+          if (!ans.getItem().find(item => item.equals(temp)))
+            ans.addItem(temp)
+        }
+      })
+    idx++
+  }
+  return ans
+}
+
+let GOTO = (collection: LR1Collection, X: string, productionList: Production[], firstMap: Map<string, Set<string>>): LR1Collection | null => {
+  let lr1ItemCore = new LR1Collection([])
+  collection
+    .getItem()
+    .filter(value => value.getHeader() === X)
+    .forEach(value => lr1ItemCore.addItem(value.getNext() as LR1Item))
+  if (lr1ItemCore.getItem().length === 0)
+    return null
+  return CLOSURE(lr1ItemCore, productionList, firstMap)
+}
+
+let getAllLR1Collections = (productionList: Production[],
+  firstMap: Map<string, Set<string>>,
+  terminatorSet: Set<string>,
+  nonTerminatorSet: Set<string>): LR1Collection[] => {
+  let allSet: Set<string> = new Set()
+  terminatorSet.forEach(v => allSet.add(v))
+  nonTerminatorSet.forEach(v => allSet.add(v))
+  let ans: LR1Collection[] = []
+  ans.push(CLOSURE(new LR1Collection([new LR1Item(productionList[0], 0, '')]), productionList, firstMap))
+  let idx = 0
+  while (idx < ans.length) {
+    let currentCollection = ans[idx]
+    for (let [X, _X] of allSet.entries()) {
+      let result = GOTO(currentCollection, X, productionList, firstMap)
+      if (result === null) {
+        continue
+      }
+      if (!ans.find(value => value.equals(result as LR1Collection))) {
+        ans.push(result)
+      }
+    }
+    console.log(`index: ${idx}, length: ${ans.length}`)
+    idx++
   }
   return ans
 }
@@ -112,7 +183,7 @@ export let parseYacc = (yaccContent: string) => {
             throw new Error('Multiple or no start unit')
           }
           startUnitName = wordsList[1]
-          productionList.push({ left: '__SEU_YACC_START', right: [startUnitName] })
+          productionList.push(new Production('__SEU_YACC_START', [startUnitName]))
           nonTerminatorSet.add('__SEU_YACC_START')
         }
         else if (wordsList[1] === '%left') {
@@ -213,10 +284,7 @@ export let parseYacc = (yaccContent: string) => {
             } while (yaccLines[currentLine][currentChar] === ' ' || yaccLines[currentLine][currentChar] === '\t')
           }
           nonTerminatorSet.add(leftItemName)
-          productionList.push({
-            left: leftItemName,
-            right: currentProduction
-          })
+          productionList.push(new Production(leftItemName, currentProduction))
           if (yaccLines[currentLine][currentChar] === ';') {
             break
           }
@@ -234,10 +302,7 @@ export let parseYacc = (yaccContent: string) => {
             }
           } while (yaccLines[currentLine][currentChar] === ' ' || yaccLines[currentLine][currentChar] === '\t')
           if (yaccLines[currentLine][currentChar] === ';') {
-            productionList.push({
-              left: leftItemName,
-              right: ['']
-            })
+            productionList.push(new Production(leftItemName, ['']))
           }
         }
         break
@@ -248,5 +313,7 @@ export let parseYacc = (yaccContent: string) => {
     currentLine++
   }
   let first = FIRST(productionList, nonTerminatorSet, terminatorSet)
-  let follow = FOLLOW(productionList, nonTerminatorSet, terminatorSet, first, '__SEU_YACC_START')
+  let result = getAllLR1Collections(productionList, first, terminatorSet, nonTerminatorSet)
+  // let c = CLOSURE(new LR1Collection([new LR1Item(productionList[0], 0, '')]), productionList, first)
+  // let follow = FOLLOW(productionList, nonTerminatorSet, terminatorSet, first, '__SEU_YACC_START')
 }
--- src/seuyacc/YaccTypes.ts
+++ src/seuyacc/YaccTypes.ts
@@ -1,4 +1,110 @@
-export type Production = {
-  left: string
-  right: string[]
+export class Production {
+  private left: string
+  private right: string[]
+
+  constructor(left: string, right: string[]) {
+    this.left = left
+    this.right = right
+  }
+
+  public getLeft(): string {
+    return this.left
+  }
+
+  public getRight(): string[] {
+    return this.right
+  }
+
+  public setLeft(left: string): void {
+    this.left = left
+  }
+
+  public setRight(right: string[]): void {
+    this.right = right
+  }
+
+  public equals(another: Production): boolean {
+    return (
+      this.left === another.left &&
+      this.right.length === another.right.length &&
+      this.right.every((value, index) => another.right[index] === value)
+    )
+  }
+
+  public deepCopy(): Production {
+    return new Production(this.left, this.right.map(item => item))
+  }
+}
+
+export class LR1Item {
+  private production: Production
+  private dot: number
+  private expect: string
+
+  constructor(production: Production, dot: number, expect: string) {
+    this.production = production
+    this.dot = dot
+    this.expect = expect
+  }
+
+  public equals(another: LR1Item): boolean {
+    return (
+      this.dot === another.dot &&
+      this.production.equals(another.production) &&
+      this.expect === another.expect
+    )
+  }
+
+  public deepCopy(): LR1Item {
+    return new LR1Item(this.production.deepCopy(), this.dot, this.expect)
+  }
+
+  public getHeader(): string | null {
+    if (this.dot < this.production.getRight().length)
+      return this.production.getRight()[this.dot]
+    else {
+      return null
+    }
+  }
+  
+  public getExpect(): string {
+    return this.expect
+  }
+
+  public getDot(): number {
+    return this.dot
+  }
+
+  public getNext(): LR1Item | null {
+    // This shouldn't happen
+    if (this.dot >= this.production.getRight().length) {
+      return null
+    }
+    return new LR1Item(this.production, this.dot + 1, this.expect)
+  }
+}
+
+export class LR1Collection {
+  private items: LR1Item[]
+
+  constructor(items: LR1Item[]) {
+    this.items = [...items]
+  }
+
+  public getItem(): LR1Item[] {
+    return this.items
+  }
+
+  public addItem(item: LR1Item): void {
+    this.items.push(item)
+  }
+
+  public deepCopy(): LR1Collection {
+    return new LR1Collection(this.items.map(item => item.deepCopy()))
+  }
+
+  public equals(another: LR1Collection): boolean {
+    return this.items.length === another.items.length && this.items.every(item => another.items.find(i => i.equals(item)))
+  }
+
 }
