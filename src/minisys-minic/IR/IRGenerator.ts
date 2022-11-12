7fcf8ed2004891246c0f2dc0e4ec84ee96138995
潘昱辰 <2718911600@qq.com>
2022-11-12 21:15:35 +0800

    添加翻译部分

--- src/minisys-minic/IR/IRGenerator.ts
+++ src/minisys-minic/IR/IRGenerator.ts
@@ -14,13 +14,17 @@ export class IRGenerator {
   private quadruples: Quadruple[]
   private scopeStack: number[]
   private scopeId: number[]
+  private scopeCount: number
+  private variableCount: number
 
   constructor() {
     this.functions = []
     this.variables = []
     this.quadruples = []
-    this.scopeStack = []
+    this.scopeStack = [0]
     this.scopeId = []
+    this.scopeCount = 1
+    this.variableCount = 0
   }
 
   debugInfo() {
@@ -29,8 +33,24 @@ export class IRGenerator {
     console.log(this.quadruples)
   }
 
+  getVariableByName(vName: string): IRVarialble {
+    const currentScope = [...this.scopeStack]
+    let scopes: number[][] = []
+    while (currentScope.length) {
+      scopes.push([...currentScope])
+      currentScope.pop()
+    }
+    for (let scope of scopes) {
+      for (let v of this.variables) {
+        if (v.name === vName && scope.length === v.scope.length && scope.every((s, i) => s === v.scope[i]))
+          return v
+      }
+    }
+    throw new Error(`未找到变量${vName}`)
+  }
+
   newVariableId(): string {
-    return `__MiniC_Variable_${this.variables.length}`
+    return `__MiniC_Variable_${this.variableCount++}`
   }
 
   public start(node: ASTNode) {
@@ -99,12 +119,17 @@ export class IRGenerator {
     const exitLabel = `__MiniC_Exit_${id}`
 
     this.functions.push(new IRFunction(id, returnType, entryLabel, exitLabel))
-    this.quadruples.push(new Quadruple('setLabel', '', '', entryLabel))
     this.parseParams(node.child[3], id)
+    if (node.child[5].label === ';') {
+      return
+    }
+    this.scopeStack.push(this.scopeCount++)
+    this.quadruples.push(new Quadruple('setLabel', '', '', entryLabel))
+    this.parseCompoundStmt(node.child[5])
     this.quadruples.push(new Quadruple('setLabel', '', '', exitLabel))
-    this.scopeStack.push(this.scopeStack.length)
 
     this.scopeStack.pop()
+    return
   }
   
   parseFunctionIdent(node: ASTNode) {
@@ -175,7 +200,19 @@ export class IRGenerator {
   }
 
   parseExprStmt(node: ASTNode) {
-
+    switch(node.child[1].label) {
+      case '=':
+        const left = this.getVariableByName(node.child[0].attributes)
+        const right = this.parseExpr(node.child[2])
+        this.quadruples.push(new Quadruple('assign', right, '', left.id))
+        break
+      case '[':
+        break
+      case 'expr':
+        break
+      case '(':
+        break
+    }
   }
 
   parseWhileStmt(node: ASTNode) {
@@ -191,15 +228,28 @@ export class IRGenerator {
   }
 
   parseCompoundStmt(node: ASTNode) {
-
+    this.parseLocalDecls(node.child[1])
+    this.parseStmtList(node.child[2])
   }
 
   parseLocalDecls(node: ASTNode) {
-
+    if (node.child.length === 0)
+      return
+    if (node.child[0].label === 'local_decls') {
+      this.parseLocalDecls(node.child[0])
+      this.parseLocalDecl(node.child[1])
+    }
+    else {
+      this.parseLocalDecl(node.child[0])
+    }
   }
 
   parseLocalDecl(node: ASTNode) {
-
+    const type = this.parseTypeSpec(node.child[0]) as MiniCType
+    const id = node.child[1].attributes as string
+    if (this.variables.some(v => v.name === id && v.scope.length === this.scopeStack.length && v.scope.every((v, i) => v === this.scopeStack[i])))
+      throw new Error('重复定义的变量')
+    this.variables.push(new IRVarialble(this.newVariableId(), id, type, [...this.scopeStack]))
   }
 
   parseIfStmt(node: ASTNode) {
@@ -211,11 +261,62 @@ export class IRGenerator {
   }
 
   parseExpr(node: ASTNode) {
+    // 整数字面量
+    if (node.child[0].label === 'int_literal') {
+      const resultVariable = this.newVariableId()
+      this.quadruples.push(new Quadruple('assignConst', this.parseIntLiteral(node.child[0]).toString(), '', resultVariable))
+      return resultVariable
+    }
 
+    // 括号表达式
+    if (node.child[0].label === '(') {
+      const result = this.parseExpr(node.child[1])
+      const target = this.newVariableId()
+      this.quadruples.push(new Quadruple('assign', result, '', target))
+      return target
+    }
+
+    // 其他变量赋值
+    if (node.child[0].label === 'IDENT') {
+      if (node.child.length === 1) {
+        const target = this.getVariableByName(node.child[0].attributes)
+        return target.id
+      }
+      else {
+        if (node.child[1].label === '[') {
+
+        }
+        else if (node.child[1].label === '(') {
+          // TODO
+        }
+        else if (node.child[1].label === '[') {
+          const result = this.parseExpr(node.child[2])
+          const target = this.newVariableId()
+          this.quadruples.push(new Quadruple('[]', this.getVariableByName(node.child[0].attributes).id, result, target))
+        }
+      }
+    }
+
+    // 一元运算
+    if (node.child.length === 2) {
+      const result = this.parseExpr(node.child[1])
+      const target = this.newVariableId()
+      this.quadruples.push(new Quadruple(node.child[0].label, result, '', target))
+      return target
+    }
+
+    if (node.child.length === 3) {
+      const left = this.parseExpr(node.child[0])
+      const right = this.parseExpr(node.child[2])
+      const target = this.newVariableId()
+      this.quadruples.push(new Quadruple(node.child[1].label, left, right, target))
+    }
+    // 二元运算
+    throw new Error()
   }
 
   parseIntLiteral(node: ASTNode) {
-
+    return node.child[0].attributes as number
   }
 
   parseArgList(node: ASTNode) {
